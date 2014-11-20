1baea97b0242b3798884b04314edd7c42df5eaee
NoemiTralara <noemi_libra8@hotmail.com>
2014-11-20 09:09:25 +0100

    prueba
    sjngosnbg

---
+++ IniciarOracleTODO.bat
@@ -0,0 +1,11 @@
+oracle.exe
+sc start OracleVssWriterORCL
+sc start OracleDBConsoleorcl
+sc start OracleJobSchedulerORCL
+sc start OracleMTSRecoveryService
+sc start OracleOraDb11g_home2ClrAgent
+sc start OracleOraDb11g_home2TNSListener
+sc start OracleServiceORCL
+cls
+C:\ProgramasModulo\Oracle\product\11.2.0\dbhome_1\BIN\sqlplus.exe 
+exit
