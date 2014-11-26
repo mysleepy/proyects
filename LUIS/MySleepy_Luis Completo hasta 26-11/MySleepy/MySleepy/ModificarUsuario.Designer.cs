4e2192de3bc2951d66d511e91a946c501d7b0a61
LuisitoDes <luisdiazchaparro@gmail.com>
2014-11-26 01:01:48 +0100

    FORMULARIOS
    Buenas noches, lo primero he organizado toda la información medio la he
    colocado en carpetas para que no este todo desorganizado vais a tener 3
    carpetas con nuestros nombres. En mi carpeta he dejado una versión del
    proyecto con bastantes cosas cambiadas si queréis seguir con esa ya que
    me parece que es la mas actualizada.

---
+++ LUIS/MySleepy_Luis Completo hasta 26-11/MySleepy/MySleepy/ModificarUsuario.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace MySleepy
+{
+    partial class ModificarUsuario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPassAnterior = new System.Windows.Forms.TextBox();
+            this.txtPassNueva = new System.Windows.Forms.TextBox();
+            this.txtPassRepetir = new System.Windows.Forms.TextBox();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Password anterior";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nueva Password";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(45, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Repita Password";
+            // 
+            // txtPassAnterior
+            // 
+            this.txtPassAnterior.Location = new System.Drawing.Point(142, 32);
+            this.txtPassAnterior.Name = "txtPassAnterior";
+            this.txtPassAnterior.Size = new System.Drawing.Size(125, 20);
+            this.txtPassAnterior.TabIndex = 3;
+            // 
+            // txtPassNueva
+            // 
+            this.txtPassNueva.Location = new System.Drawing.Point(142, 86);
+            this.txtPassNueva.Name = "txtPassNueva";
+            this.txtPassNueva.PasswordChar = '*';
+            this.txtPassNueva.Size = new System.Drawing.Size(125, 20);
+            this.txtPassNueva.TabIndex = 4;
+            // 
+            // txtPassRepetir
+            // 
+            this.txtPassRepetir.Location = new System.Drawing.Point(142, 137);
+            this.txtPassRepetir.Name = "txtPassRepetir";
+            this.txtPassRepetir.PasswordChar = '*';
+            this.txtPassRepetir.Size = new System.Drawing.Size(125, 20);
+            this.txtPassRepetir.TabIndex = 5;
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(318, 30);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(75, 23);
+            this.btnAceptar.TabIndex = 6;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(318, 86);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 7;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // ModificarUsuario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(433, 184);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.txtPassRepetir);
+            this.Controls.Add(this.txtPassNueva);
+            this.Controls.Add(this.txtPassAnterior);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ModificarUsuario";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPassAnterior;
+        private System.Windows.Forms.TextBox txtPassNueva;
+        private System.Windows.Forms.TextBox txtPassRepetir;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Button btnCancelar;
+    }
+}
