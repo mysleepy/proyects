12eb5b4e91ad76324708ab0afc70171916847094
Costex <yodce1@gmail.com>
2014-12-07 17:55:54 +0100

    Proyecto con clase XML
    En fase de contruccion XML_proveedor, Aunque estoy intentando que los
    metodos sean lo más genericos posibles para reutilizarlos

--- CARLOS/MySleepy_Carlos/MySleepy/MySleepy/XML_proveedor.cs
+++ CARLOS/MySleepy_Carlos/MySleepy/MySleepy/XML_proveedor.cs
@@ -11,7 +11,15 @@ namespace MySleepy
 {
     class XML_proveedor
     {
-        public static void Leerxml(String direccionXml,String tabla)
+        /// <summary>
+        /// Metodo que lee el archivo indicado y devuelve un
+        /// array de String con los valores de los campos
+        /// </summary>
+        /// <param name="direccionXml">ruta del archivo a leer</param>
+        /// <param name="tabla">tabla a leer de la BBDD</param>
+        /// <param name="campos">cantidad de campos que contiene la tabla a leer</param>
+        /// <returns>un array de String con todos los nodos</returns>
+        public static String[,] Leerxml(String direccionXml,String tabla,int campos)
         {
             XmlDataDocument xmldoc = new XmlDataDocument();
             XmlNodeList xmlnode;
@@ -20,12 +28,62 @@ namespace MySleepy
             FileStream fs = new FileStream(direccionXml, FileMode.Open, FileAccess.Read);
             xmldoc.Load(fs);
             xmlnode = xmldoc.GetElementsByTagName(tabla);
-            resultado = new String[xmlnode.Count,];
+            resultado = new String[xmlnode.Count,campos];
             for (i = 0; i < xmlnode.Count - 1; i++)
             {
                 xmlnode[i].ChildNodes.Item(0).InnerText.Trim();
-                
+                for (int j = 0; j < campos; j++)
+                {
+                    resultado[i, j] = xmlnode[i].ChildNodes.Item(j).InnerText.Trim();
+                }
             }
+            return resultado;
+        }
+        /// <summary>
+        /// Metodo que escribira en el archivo XML especificado
+        /// </summary>
+        /// <param name="rutaXml">ruta en la que se encuentra o se creara el archivo</param>
+        /// <param name="nombreElemento">Nombre usado para los nodos</param>
+        /// <param name="nombreColumnas">Nombres usados para los nodos hijos</param>
+        /// <param name="valorCampos">Valores a almacenar en cada nodo hijo</param>
+        public static void escribirXml(String rutaXml, String nombreElemento,String[] nombreColumnas,String[,] valorCampos)
+        {
+            XmlTextWriter writer = new XmlTextWriter(rutaXml, System.Text.Encoding.UTF8);
+            writer.WriteStartDocument(true);
+            writer.Formatting = Formatting.Indented;
+            writer.Indentation = 2;
+            writer.WriteStartElement("Table");
+            for (int i = 0; i < valorCampos.GetUpperBound(0); i++)
+            {
+                String[] registro = new String[valorCampos.GetUpperBound(1)];
+                for (int j = 0; j < registro.Length; j++)
+                {
+                    registro[j] = valorCampos[i, j];                  
+                }
+                crearNodo(writer, nombreElemento, nombreColumnas, registro);
+            }
+            writer.WriteEndElement();
+            writer.WriteEndDocument();
+            writer.Close();
+        }
+        /// <summary>
+        /// Metodo que genera los nodos para almacenar los 
+        /// datos en el archivo xml
+        /// </summary>
+        /// <param name="writer">Clase usada para escribir en el archivo XML</param>
+        /// <param name="nombreElemento">Nombre usado para los nodos</param>
+        /// <param name="nombreColumnas">Nombres para los nodos hijo (ChildNode)</param>
+        /// <param name="valorCampos">Valores a almacenar en cada nodo hijo</param>
+        private static void crearNodo(XmlTextWriter writer,String nombreElemento, String[] nombreColumnas, String[] valorCampos)
+        {
+            writer.WriteStartElement(nombreElemento);
+            for (int i = 0; i < nombreColumnas.Length; i++)
+            {
+                writer.WriteStartElement(nombreColumnas[i]);
+                writer.WriteString(valorCampos[i]);
+                writer.WriteEndElement();
+            }
+            writer.WriteEndElement();
         }
     }
 }
