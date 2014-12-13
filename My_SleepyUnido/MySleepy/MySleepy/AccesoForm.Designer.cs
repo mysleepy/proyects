86ef5d4282862dd2d7534df011bb1b0e1828225c
Costex <yodce1@gmail.com>
2014-12-13 20:27:59 +0100

    Cursor Cambiado

--- My_SleepyUnido/MySleepy/MySleepy/AccesoForm.Designer.cs
+++ My_SleepyUnido/MySleepy/MySleepy/AccesoForm.Designer.cs
@@ -135,7 +135,7 @@
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.textBox1);
-            this.groupBox1.Font = new System.Drawing.Font("Mistral", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox1.Location = new System.Drawing.Point(242, 335);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(223, 141);
@@ -172,6 +172,7 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Cursor = System.Windows.Forms.Cursors.WaitCursor;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "AccesoForm";
